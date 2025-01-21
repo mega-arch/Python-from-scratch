70b2d7279dfff1d6420f7e343647f72a7151b955
Megavarshini <136808412+mega-arch@users.noreply.github.com>
2025-01-21 23:32:38 +0530

    P14.py
    Without using append()

--- P14.py
+++ P14.py
@@ -1,10 +1,15 @@
 # Merge two arrays
 def merge_arrays(arr1, arr2): #2 parameters for 2 arrays to be merged 
     merged = [] #create empty array
-    for num in arr1: 
-        merged.append(num)
-    for num in arr2:
-        merged.append(num)
-    return merged
+    index = 0 #position of each element in array 
+    for i in range(len(arr1)): #length of arr1 is range of iteration
+        merged[index] = arr1[i] #ith elemnet of arr1 is assigned to merged array intially 0 th index has ith element of arr1
+        index += 1
+        
+    for i in range(len(arr2)):
+        merged_array[index] = arr2[i]
+        index += 1
 
 print(merge_arrays([1, 2, 3], [4, 5, 6]))  # Output: [1, 2, 3, 4, 5, 6]
+
+    
