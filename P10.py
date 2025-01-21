125dd488811e9f770533a122b7f79af382f79880
Megavarshini <136808412+mega-arch@users.noreply.github.com>
2025-01-21 22:58:29 +0530

    P10.py

--- P10.py
+++ P10.py
@@ -1,11 +1,10 @@
 # Count the number of vowels in a string
 def count_vowels(s):
-    vowels = "aeiou"
+    vowels = "aeiou" #declaring vowels in seperate variable
     count = 0
-    for char in s:
+    for char in s: #in each iteration checks individual character 
         if char.lower() in vowels:
             count += 1
     return count
 
-# Test
 print(count_vowels("hello world"))  # Output: 3
