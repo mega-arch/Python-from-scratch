a7d98644c677ac37f6da3a47130f66fcace5592d
Megavarshini <136808412+mega-arch@users.noreply.github.com>
2025-01-21 23:05:37 +0530

    P13.py

--- P13.py
+++ P13.py
@@ -1,9 +1,8 @@
 # Check if an element is in the array
-def element_in_array(arr, element):
-    for i in range(len(arr)):
-        if arr[i] == element:
+def element_in_array(arr, element): #both array and the element to be checked is given in paramater
+    for i in range(len(arr)): #length of the array is set as range
+        if arr[i] == element: #checks if it is there 
             return True
     return False
-
-# Test
+    
 print(element_in_array([1, 2, 3, 4], 3))  # Output: True
