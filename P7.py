1b8d355e57bc872e096cdf629d217a23b1c505af
Megavarshini <136808412+mega-arch@users.noreply.github.com>
2025-01-22 00:08:13 +0530

    P7.py

--- P7.py
+++ P7.py
@@ -1,12 +1,10 @@
-# Implement Bubble Sort
-def bubble_sort(arr):
-    n = len(arr)
-    for i in range(n):
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
-                # Swap if the element found is greater than the next element
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-    return arr
+# Find n power of a number
+def power(base,exp):
+    i=1 #used to keep the while loop going 
+    res=1
+    while i<=exp: #until i is less or equal to exp value the condition is true and loop executes 
+        res=res*base # base is constant but for each power increment the value changes 
+        i+=1
+    return res
 
-# Test
-print(bubble_sort([64, 34, 25, 12, 22, 11, 90]))  # Output: [11, 12, 22, 25, 34, 64, 90]
+print(power(2,3))  # Output: 8
